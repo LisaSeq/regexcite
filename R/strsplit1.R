79c166cfccb422895934969834deda8b31a11bec
LisaSeq <112457048+LisaSeq@users.noreply.github.com>
2023-01-22 02:18:10 -0800

    Edited description, MIT license and added roxygen header to strsplit()

--- R/strsplit1.R
+++ R/strsplit1.R
@@ -1,5 +1,14 @@
-#> • Edit 'R/strsplit1.R'
-#> • Call `use_test()` to create a matching test file
+#' Split a string
+#'
+#' @param x A character vector with one element.
+#' @param split What to split on.
+#'
+#' @return A character vector.
+#' @export
+#'
+#' @examples
+#' #' x <- "alfa,bravo,charlie,delta"
+#' strsplit1(x, split = ",")
 strsplit1 <- function(x, split) {
   strsplit(x, split = split)[[1]]
 }
